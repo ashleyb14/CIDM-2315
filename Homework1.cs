d1db09fe22d565b9687cdf017588ddb74b98aa38
ashleyb14 <aborden2@buffs.wtamu.edu>
2026-01-23 18:50:00 -0600

    Homework1 code file

---
+++ Homework1.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = 4*x*x + 3*y;
+        Console.WriteLine($"x = {x}, y = {y}");
+        Console.WriteLine($"The value of Z is: {z}");
+    }
+}
